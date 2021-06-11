05dd5ee8c36ad821c7dea3a69694176bdbc08c72
kohei <shoot0321@gmail.com>
2021-06-11 17:45:22 +0900

    Q8,Q10,Q11,Q15を修正

--- task.rb
+++ task.rb
@@ -64,6 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages = programming_languages.map(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -84,10 +85,11 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
+    puts food
     if food.include?("うに")
       puts "好物です"
     else
-      puts "まあまあ好きです"
+      puts "まぁまぁ好きです"
     end
   end
 end
@@ -96,12 +98,12 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  text = "ユーザーの趣味一覧\n"
+  puts "ユーザーの趣味一覧"
   sports.flatten!
+  sports = sports.uniq
   sports.each.with_index(1) do |sport, i|
-    text += "No#{i} #{sport}\n"
+    puts "No#{i} #{sport}\n"
   end
-  puts text
 end
 
 def q12
@@ -134,11 +136,8 @@ def q15
   # 以下に回答を記載
   datas = [data1, data2]
   datas.each do |data|
-    if data.include?(:age)
-      puts "OK"
-    else
-      puts "NG"
-    end
+    data = data.include?(:age) ? "OK" : "NG"
+    puts data
   end
 end
 
