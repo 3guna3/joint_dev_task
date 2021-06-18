25b66882ff09c2bdd37c955d7f9f7b4adbcc524f
kohei <shoot0321@gmail.com>
2021-06-18 18:10:57 +0900

    Q20を修正

--- task.rb
+++ task.rb
@@ -190,7 +190,7 @@ class UserQ18
 
   def introduce
     if @age < 20
-      "はいさいまいど〜,#{@name}です！！！"
+      "はいさいまいど〜、 #{@name}です！！！"
     else
       "こんにちは、#{@name}と申します。宜しくお願いいたします。"
     end
@@ -238,16 +238,17 @@ class Zoo
   end
 
   def info_entry_fee(user)
-    case user.age
-    when 0..5
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:infant]}円です。"
-    when 6..12
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:children]}円です。"
-    when 13..64
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:adult]}円です"
-    when 65..120
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:senior]}円です。"
-    end
+    fee = case user.age
+      when 0..5
+        fee = @entry_fee[:infant]
+      when 6..12
+        fee = @entry_fee[:children]
+      when 13..64
+        fee = @entry_fee[:adult]
+      when 65..120
+        fee = @entry_fee[:senior]
+      end
+    puts "#{user.name}さんの入場料金は#{fee}円です。"
   end
 end
 
